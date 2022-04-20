c17a9b569cd4962e9d13fcf1f4c929a0f1b86faf
Benjamin Heater <benheater@gmail.com>
2022-04-20 15:51:08 -0400

    Add logic to ensure successful configuration
    Hit a snag where the script ran, but did not actually pull IPs from the CloudFlare source -- due to some kind of network failure -- thus I blocked a CloudFlare proxy IP rather than a test IP.

--- cloudflare-sync-ips.sh
+++ cloudflare-sync-ips.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-CLOUDFLARE_FILE_PATH=${1:-/etc/nginx/cloudflare}
+CLOUDFLARE_FILE_PATH=/etc/nginx/conf.d/cloudflare-real-ip.conf
 
-echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
-echo "" >> $CLOUDFLARE_FILE_PATH;
+function UpdateConfigFile {
+    echo "# Configuration file to enable the logging of real client IP" > $CLOUDFLARE_FILE_PATH;
+    echo "# Cloudflare proxies send a CF-Connecting-IP header that can be logged." >> $CLOUDFLARE_FILE_PATH;
+    echo "# Cloudflare Official Proxy IP Addressses" >> $CLOUDFLARE_FILE_PATH;
+    echo "" >> $CLOUDFLARE_FILE_PATH;
 
-echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl -s -L https://www.cloudflare.com/ips-v4`; do
+    echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
+    for i in `curl -s -L https://www.cloudflare.com/ips-v4`; do
         echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
-done
+    done
 
-echo "" >> $CLOUDFLARE_FILE_PATH;
-echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl -s -L https://www.cloudflare.com/ips-v6`; do
+    echo "" >> $CLOUDFLARE_FILE_PATH;
+    echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
+    for i in `curl -s -L https://www.cloudflare.com/ips-v6`; do
         echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+    done
+
+    echo "" >> $CLOUDFLARE_FILE_PATH;
+    echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
+}
+
+UpdateConfigFile
+
+# Ensure the operation succeeded by checking for the set_real_ip line
+while ! grep set_real_ip /etc/nginx/conf.d/cloudflare-real-ip.conf > /dev/null;
+do
+    sleep 3
+    UpdateConfigFile
 done
-
-echo "" >> $CLOUDFLARE_FILE_PATH;
-echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
-
-#test configuration and reload nginx
-nginx -t && systemctl reload nginx
